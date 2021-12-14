d7e46f2e8aa753e73809fe30a810972fa942525f
menef0124 <menefee0124@gmail.com>
2021-12-14 15:13:37 -0600

    More server setup and added empty templates

--- app.js
+++ app.js
@@ -10,7 +10,30 @@ User = require('./models/user');
 
 const PORT = 8080;
 
+
+
+let genRandString = function (len){
+    let txt = '';
+    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for(let i=0;i<len;i++){
+        txt += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return txt;
+};
+
+let stateKey = 'spotify_auth_state';
+
 let app = express();
+
+//Express server setup
+app.use(express.static(path.join(__dirname, 'static')))
+    .use(express.urlencoded({extended: false}))
+    .use(session({secret: 'superSecret', resave: false, saveUninitialized: false}))
+    .use(cors())
+    .use(cookieParser())
+    .set('view engine', 'handlebars')
+    .engine('handlebars', hbs());
 
 app.get('/', (req, res) =>{
     console.log("Hello");
